dd91988423b657c5d8a327802a18b3e66bb6fc91
cblaney95 <Cblaney1995@gmail.com>
2018-05-03 13:54:19 -0400

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -10,75 +11,181 @@
  *
  * @author Chris
  */
-public class Calculator {    
-    private double answer;
-    private double[] equation = new double[3];
-    
-    public void calculate() {
+public class Calculator {
+    public String[] equation;
+    public ArrayList<String> lEquation;
+    public void calculate() {        
         while(true) {
             this.equation = Parser.getLine();
             if(this.equation == null) {
                 break;
-            } else {
-                switch((int)(equation[1])) {
-                    //addition
-                    case 0:
-                        addition();
-                        break;
-                    //subtraction
-                    case 1:
-                        subtraction();
-                        break;
-                    //multiplication
-                    case 2:
-                        multiplication();
-                        break;
-                    //division
-                    case 3:
-                        division();
-                        break;
-                }
-                display();
             }
+            fillList();
+            printList();
+            solveEq();
+        }        
+    }
+    //there is a bug
+    //needs to read from left to right in situations where there are equal
+    //precedented operators
+    public void solveEq() {
+        while(true) {
+            if(lEquation.size() == 1) {
+                break;
+            }
+            int i = findOp();
+            if(isMult(i)) {
+                String t = multiply(Double.parseDouble(lEquation.get(i - 1)), Double.parseDouble(lEquation.get(i + 1))); 
+                lEquation.set((i - 1), t);
+                lEquation.remove(i + 1);
+                lEquation.remove(i);
+                printList();
+            } else if(isDiv(i)) {
+                String t = divide(Double.parseDouble(lEquation.get(i - 1)), Double.parseDouble(lEquation.get(i + 1))); 
+                lEquation.set((i - 1), t);
+                lEquation.remove(i + 1);
+                lEquation.remove(i);
+                printList();
+            } else if(isSub(i)) {
+                String t = subtract(Double.parseDouble(lEquation.get(i - 1)), Double.parseDouble(lEquation.get(i + 1))); 
+                lEquation.set((i - 1), t);
+                lEquation.remove(i + 1);
+                lEquation.remove(i);
+                printList();
+            } else if(isAdd(i)) {
+                String t = add(Double.parseDouble(lEquation.get(i - 1)), Double.parseDouble(lEquation.get(i + 1))); 
+                lEquation.set((i - 1), t);
+                lEquation.remove(i + 1);
+                lEquation.remove(i);
+                printList();
+            }
+            /*int i = isMult();
+            if(i != -1) {
+                String t = multiply(Double.parseDouble(lEquation.get(i - 1)), Double.parseDouble(lEquation.get(i + 1))); 
+                lEquation.set((i - 1), t);
+                lEquation.remove(i + 1);
+                lEquation.remove(i);
+                //lEquation.trimToSize();
+                printList();
+                continue;
+            }
+            i = isDiv();
+            if(i != -1) {
+                String t = divide(Double.parseDouble(lEquation.get(i - 1)), Double.parseDouble(lEquation.get(i + 1))); 
+                lEquation.set((i - 1), t);
+                lEquation.remove(i + 1);
+                lEquation.remove(i);
+                //lEquation.trimToSize();
+                printList();
+                continue;
+            }
+            i = isAdd();
+            if(i != -1) {
+                String t = add(Double.parseDouble(lEquation.get(i - 1)), Double.parseDouble(lEquation.get(i + 1)));
+                lEquation.set((i - 1), t);
+                lEquation.remove(i + 1);
+                lEquation.remove(i);
+                //lEquation.trimToSize();
+                printList();
+                continue;
+            }
+            i = isSub();
+            if(i != -1) {
+                String t = subtract(Double.parseDouble(lEquation.get(i - 1)), Double.parseDouble(lEquation.get(i + 1))); 
+                lEquation.set((i - 1), t);
+                lEquation.remove(i + 1);
+                lEquation.remove(i);
+                //lEquation.trimToSize();
+                printList();
+            }*/
+        }  
+    }
+    
+    public int findOp() {
+        int index = -1;
+        for(int i = 0; i < lEquation.size(); i++) {
+           if(lEquation.get(i).equals("*") || lEquation.get(i).equals("/")) {
+               return i;
+           }  
+        }
+        for(int i = 0; i < lEquation.size(); i++) {
+            if(lEquation.get(i).equals("+") || lEquation.get(i).equals("-")) {
+               return i;
+           }
+        }
+        //System.out.printf("Found %s at %d\n", lEquation.get(index), index);
+        return index;
+    }
+    
+    public boolean isMult(int index) {    
+        boolean flag = false;
+        if(lEquation.get(index).equals("*")) {
+            flag = true;
+        }
+        return flag;
+    }
+    
+    public boolean isDiv(int index) {
+        boolean flag = false;
+        if(lEquation.get(index).equals("/")) {
+            flag = true;
+        }
+        return flag;
+    }
+    
+    public boolean isSub(int index) {
+        boolean flag = false;
+        if(lEquation.get(index).equals("-")) {
+            flag = true;
+        }
+        return flag;
+    }
+    
+    public boolean isAdd(int index) {
+        boolean flag = false;
+        if(lEquation.get(index).equals("+")) {
+            flag = true;
+        }
+        return flag;
+    }
+    
+    public void fillList() {
+        lEquation = new ArrayList<String>();
+        for(int i = 0; i < equation.length; i++) {
+            lEquation.add(i, equation[i]);
         }
     }
-    //this can be improved with significant figures
-    public void display() {
-        String operator =" ";               
-        switch((int)(equation[1])) {
-            case 0:
-                operator = "+";
-                break;
-            case 1:
-                operator = "-";
-                break;
-            case 2:
-                operator = "*";
-                break;
-            case 3:
-                operator = "/";
-                break;
+    
+    public void printList() {
+        for(int i = 0; i < lEquation.size(); i++) {
+            System.out.print(lEquation.get(i) + " ");
         }
-        String line = Double.toString(equation[0]) + operator + Double.toString(equation[2]) + " = " + Double.toString(answer);
-        System.out.println(line);
-                
-    }
-    
-    public void addition() {
-        this.answer = (equation[0] + equation[2]);
-    }
-    
-    public void subtraction() {
-        this.answer = (equation[0] - equation[2]);
-    }
-    
-    public void multiplication() {
-        this.answer = (equation[0] * equation[2]);
-    }
-    
-    public void division() {
-        this.answer = (equation[0] / equation[2]);
+        System.out.println();
     }
     
     
+    
+    public String multiply(double l, double r) {
+        double x = l * r;
+        String answer = Double.toString(x);
+        return answer;
+    }
+    
+    public String divide(double l, double r) {
+        double x = l / r;
+        String answer = Double.toString(x);
+        return answer;
+    }
+    
+    public String add(double l, double r) {
+        double x = l + r;
+        String answer = Double.toString(x);
+        return answer;
+    }
+    
+    public String subtract(double l, double r) {
+        double x = l - r;
+        String answer = Double.toString(x);
+        return answer;
+    }    
 }
--- Parser.java
+++ Parser.java
@@ -12,74 +12,42 @@ import java.util.Scanner;
  * @author Chris
  */
 public class Parser {
-    public static double[] getLine() {
-        Scanner input = new Scanner(System.in);
-        double[] arr = new double[3];
-        String line;
+    public static final String DELIM = "((?<=\\*)|"
+                + "(?=\\*))|((?<=/)|(?=/))|((?<=\\+)|(?=\\+))|"
+                + "((?<=-)|(?=-))";
+    public static String[] getLine() {        
+        Scanner input = new Scanner(System.in);        
+        String[] arr;
+        String line = "";
         try {
-            System.out.println("Enter an equation:");
+            System.out.println("Enter an equation (or exit):");
             line = input.nextLine();
             if(line.equals("exit")) {
                 return null;
-            } else {
-                while(!checkLine(line)) {
-                    try {
-                        //System.out.println("The format of the equation is incorrect");            
-                        System.out.println("Enter an equation:");
-                        line = input.nextLine();
-                        if(line.equals("exit")) {
-                            return null;
-                        }
-                    } catch(NumberFormatException e) {
-                        System.out.println("The format of the equation is incorrect");            
- 
-                    }
-                }
-                arr = parseEq(line);
             }
-        } catch(NumberFormatException e) {
-            System.out.println("The format of the equation is incorrect");            
-        }        
+            while(!checkLine(line)) {
+                System.out.println("Enter an equation (or exit):");
+                line = input.nextLine();
+                if(line.equals("exit")) {
+                    return null;
+                }
+            }
+        } catch(NullPointerException e) {
+            System.out.println("Null pointer exception");
+        }
+        line = line.replaceAll("\\s", "");
+        arr = line.split(DELIM);
         return arr;
     }
     
     public static boolean checkLine(String line) {
         boolean flag = false;
-        String[] arr = line.split(" ");
-        try{
-            if(arr[1].equals("+") || arr[1].equals("-") || 
-                    arr[1].equals("*") || arr[1].equals("/")) {
+        for(int i = 0; i < line.length(); i++) {
+            if(line.charAt(i) == '+' || line.charAt(i) == '-'
+                    || line.charAt(i) == '*' || line.charAt(i) == '/') {
                 flag = true;
-            } else {
-                System.out.println("The format of the equation is incorrect");
             }
-            
-        } catch(ArrayIndexOutOfBoundsException e) {
-            System.out.println("The format of the equation is incorrect");
         }
         return flag;
     }
-    
-    public static double[] parseEq(String line) {
-        double[] equation = new double[3];
-        String[] arr = line.split(" ");
-        equation[0] = Double.parseDouble(arr[0]);
-        switch(arr[1]) {
-            case "+":
-                equation[1] = 0;
-                break;
-            case "-":
-                equation[1] = 1;
-                break;
-            case "*":
-                equation[1] = 2;
-                break;
-            case "/":
-                equation[1] = 3;
-                break;            
-        }
-        equation[2] = Double.parseDouble(arr[2]);
-        return equation;
-    }
-
 }
